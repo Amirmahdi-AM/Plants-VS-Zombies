0335b0bc5ad6a18dbb6f493e11d1544e831e1bb1
ALISHA ._. <alishakerij@gmail.com>
2024-06-29 16:30:39 +0330

    Loading page was added.Images folder and source file for images was created.Sign in page was added.

--- Client/mainwindow.cpp
+++ Client/mainwindow.cpp
@@ -1,14 +1,53 @@
 #include "mainwindow.h"
 #include "ui_mainwindow.h"
-
+#include <QCoreApplication>
+#include <QTimer>
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    ui->GameControl->setFixedSize(800,800);
+
+    //////////////////////////////////////////////
+    QPixmap Loadingpic(":/Images/LoadingBG.png");
+
+    QPalette palette1;
+    palette1.setBrush(QPalette::Window, QBrush(Loadingpic));
+    ui->FLoading->setPalette(palette1);
+    ui->FLoading->setAutoFillBackground(true);
+    //////////////////////////////////////////////
+
+    ui->GameControl->setCurrentIndex(0);
+    changetab = new QTimer();
+    changetab->start(2000);
+    connect(changetab,&QTimer::timeout,this,&MainWindow::ChangetabHandle);
+
 }
 
+void MainWindow::ChangetabHandle(){
+    QPixmap Loginpic(":/Images/LoginBG.png");
+
+    QPalette palette2;
+    palette2.setBrush(QPalette::Window, QBrush(Loginpic));
+    ui->LoginPage->setPalette(palette2);
+    ui->LoginPage->setAutoFillBackground(true);
+    ui->GameControl->setCurrentIndex(1);
+    changetab->stop();
+}
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_SignUp_clicked()
+{
+    QPixmap SignUppic(":/Images/SignUpBG.png");
+
+    QPalette palette3;
+    palette3.setBrush(QPalette::Window, QBrush(SignUppic));
+    ui->Signup->setPalette(palette3);
+    ui->Signup->setAutoFillBackground(true);
+    ui->GameControl->setCurrentIndex(2);
+}
+
